195e9a68f953f09ca5bb6378074f4e9ae8d2e83e
etherscam <40215726+etherscam@users.noreply.github.com>
2020-09-29 15:16:55 +0300

    Update 1ClickKeep.py

--- 1ClickKeep.py
+++ 1ClickKeep.py
@@ -5,14 +5,12 @@ import paramiko
 from streamlit import components
 import paramiko
 import io
-
+import json
 
 st.title('KEEP ECDSA+BEACON')
-
-#ETH = st.text_input("Номер локации")
 st.markdown(("""
        <!DOCTYPE html>
-
+       
 <style>
 html {
   background: #f3ffe6;
@@ -173,11 +171,7 @@ body {
 </html>
 <!-- TradingView Widget END -->
         """),unsafe_allow_html=True)
-#st.write(first_name)
 st.markdown('<font color=‘black’>To continue, you need to create a MEW and VPS</font>', unsafe_allow_html=True)
-
-
-#ETH = st.text_input('Enter ETH adress')
 host = st.text_input('Enter ip(host)')
 user = st.text_input('Enter user(host)')
 secret = st.text_input('Enter password(host)',type="password")
@@ -189,16 +183,12 @@ if secret and user:
 
     try:
         client.connect(hostname=host, username=user, password=secret, port=port)
-        # print(data)
     except Exception:
-        #s = st.markdown('<font color=‘orange’>Try again please check pass 1</font>', unsafe_allow_html=True)
         a=0
         st.error('Try again please check user/pass or ip')
 
     else:
         f.append(client.connect)
-
-        #st.markdown('<font color=‘Black’>Congratulation</font>', unsafe_allow_html=True)
         st.success('Confirmed')
 
         h = '-h'
@@ -206,46 +196,34 @@ if secret and user:
             "ls -a"
         ]
         for command in commands:
-            #st.write("=" * 30, command, "=" * 30)
+
             stdin, stdout, stderr = client.exec_command(command)
             a = 1
-            #st.write(stdout.read().decode())
             err = stderr.read().decode()
-            #st.write(a)
-            # if err:
-            #     st.write('try')
-            # else:
-            #     st.write("try again")
         client.close()
 
     if a==1:
         import json
-        #st.write(a)
-        #st.set_option('deprecation.showfileUploaderEncoding', False)
-
-        file = st.file_uploader(label="Import (mew) json file (-UTC-)")
+        file  = st.file_uploader(label="Import (mew) json file (-UTC-)")
         if file:
             file_2 = json.load(file)
             json_string = json.dumps(file_2)
-
             lll = str(file_2)
-            #st.write(json_string)
-            #st.write(type(lll))
-            #file_1 = file_2.read()
             dictList = []
             for key, value in file_2.items():
                 print(value)
                 dictList.append(value)
             f = dictList[2]
-            st.write(dictList)
+            
             ETH = str('0x') + f
             st.success(ETH)
-            #st.write(lll)
         if file:
             pass_eth = st.text_input('Enter password(Wallet JSON)', type="password")
             p = []
             if file and pass_eth:
                 from web3.auto import w3
+                import web3
+                from web3 import Web3
 
                 try:
                     private_key = w3.eth.account.decrypt(file_2,
@@ -253,94 +231,129 @@ if secret and user:
                 except:
                     st.error('Try again please check pass for wallet')
                 else:
-                    st.success('Confirmed')
-                    p.append(private_key)
-                    if st.button('Run'):
-                        #st.write ('GO')
-                        client = paramiko.SSHClient()
-                        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                        client.connect(hostname=host, username=user, password=secret, port=port)
-                        h = '-h'
-                        commands = [
-                            "sudo apt update",
-                            "yes | sudo apt install git",
-                            "sudo apt install docker.io curl -y",
-                            "sudo systemctl start docker",
-                            "sudo systemctl enable docker",
-                            "sudo ufw allow 22 && sudo ufw allow 3919 && sudo ufw allow 3920 && yes | sudo ufw enable",
-                            "sudo ufw status",
+                    ropsten_url = "https://ropsten.infura.io/v3/xxxxxxxxxxx"
+                    web3 = Web3(Web3.HTTPProvider(ropsten_url))
+                    with open('TokenGrant.json') as r:
+                        data = json.load(r)
+                    abi = data["abi"]
+                    address = web3.toChecksumAddress("0xb64649fe00f8Ef5187d09d109C6F38f13C7CF857")
+                    contract = web3.eth.contract(address=address, abi=abi)
+                    grand = contract.functions.getGrants(web3.toChecksumAddress(ETH)).call()
+                    #st.write(grand)
+                    if not grand:
+                        st.error('Grand not received go https://us-central1-keep-test-f3e0.cloudfunctions.net/keep-faucet-ropsten?account=%s' % ETH)
+                    else:
+                        st.success('Grand received')
 
-                            "git clone https://github.com/icohigh/keep-nodes.git",
-                            "echo '%s' >> $HOME/keep-nodes/data/eth-address.txt" % ETH,
-                            "echo '%s' >> $HOME/keep-nodes/data/eth-address-pass.txt" % pass_eth,
-                            "echo '%s' >> $HOME/keep-nodes/data/keep_wallet.json" % json_string,
-                            "echo 'export ETH_PASSWORD=$(cat $HOME/keep-nodes/data/eth-address-pass.txt)' >> $HOME/.profile",
-                            "echo 'export SERVER_IP=$(curl ifconfig.co)' >> $HOME/.profile",
-                            "grep -rl INFURA_BEACON_ID $HOME/keep-nodes/beacon/config* | xargs perl -p -i -e 's/INFURA_BEACON_ID/df8574df74084c71a997f56f137562d0/g'",
-                            "grep -rl INFURA_ECDSA_ID $HOME/keep-nodes/ecdsa/config* | xargs perl -p -i -e 's/INFURA_ECDSA_ID/ab706352c72543af96db73d3b38edad4/g'",
-                            "sed -i 's/.*URL = .*/URL = \"wss:\/\/ropsten.pfk2020.top\/wss\"/g' $HOME/keep-nodes/beacon/config/config.toml",
-                            "sed -i 's/.*URLRPC = .*/URLRPC = \"https:\/\/ropsten.pfk2020.top\/rpc\"/g' $HOME/keep-nodes/beacon/config/config.toml",
-                            "sed -i 's/.*URL = .*/URL = \"wss:\/\/ropsten.pfk2020.top\/wss\"/g' $HOME/keep-nodes/ecdsa/config/config.toml",
-                            "sed -i 's/.*URLRPC = .*/URLRPC = \"https:\/\/ropsten.pfk2020.top\/rpc\"/g' $HOME/keep-nodes/ecdsa/config/config.toml",
-                            "export ETH_PASSWORD=$(cat $HOME/keep-nodes/data/eth-address-pass.txt)",
-                            "sudo docker run -d \
-                        --entrypoint /usr/local/bin/keep-client \
-                        --restart always \
-                        --volume $HOME/keep-nodes/data:/mnt/data \
-                        --volume $HOME/keep-nodes/beacon/config:/mnt/beacon/config \
-                        --volume $HOME/keep-nodes/beacon/persistence:/mnt/beacon/persistence \
-                        --env KEEP_ETHEREUM_PASSWORD=%s \
-                        --env LOG_LEVEL=debug \
-                        --name keep-client \
-                        -p 3919:3919 \
-                        keepnetwork/keep-client:v1.3.0-rc.4 --config /mnt/beacon/config/config.toml start" %pass_eth,
-                            "sudo docker run -d \
-                        --entrypoint /usr/local/bin/keep-ecdsa \
-                        --restart always \
-                        --volume $HOME/keep-nodes/data:/mnt/data \
-                        --volume $HOME/keep-nodes/ecdsa/config:/mnt/ecdsa/config \
-                        --volume $HOME/keep-nodes/ecdsa/persistence:/mnt/ecdsa/persistence \
-                        --env KEEP_ETHEREUM_PASSWORD=%s \
-                        --env LOG_LEVEL=debug \
-                        --name keep-ecdsa \
-                        -p 3920:3919 \
-                        keepnetwork/keep-ecdsa-client:v1.2.0-rc.5 --config /mnt/ecdsa/config/config.toml start" %pass_eth
-                        ]
+                    stake = contract.functions.stakeBalanceOf(web3.toChecksumAddress(ETH)).call()
+                    if stake != 0 and stake > 90000:
+                        st.success('KEEP Tokens delegated')
+                    else:
+                        st.error('Please delegate KEEP go https://dashboard.test.keep.network/tokens/delegate')
 
-                        with st.spinner('Wait ... please'):
-                            for command in commands:
-                                #st.write("-" * 2,command)
-                                stdin, stdout, stderr = client.exec_command(command)
-                                print(stdout.read().decode())
-                                err = stderr.read().decode()
-                                if err:
-                                    print(err)
+                    with open('KeepRandomBeaconOperator.json') as r:
+                        data = json.load(r)
+                    abi = data["abi"]
+                    address = web3.toChecksumAddress("0xC8337a94a50d16191513dEF4D1e61A6886BF410f")
 
-                            # print(data)
-                            client.close()
-                        st.success('Congratulations, the node is up and running! (please check ECDSA logs on the server itself)')
-                        st.success('Paste on server: sudo docker logs keep-client -f --since 1m')
-                        #ALTERNATIV without SED
-                        # ssh = paramiko.SSHClient()
-                        # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                        # ssh.connect(host,  username=user, password=secret)
-                        # sftp = ssh.open_sftp()
-                        # # sftp.put('/root/keep-nodes/beacon/','data.txt')
-                        # # source= 'config.toml'
-                        # # destination ='/root/keep-nodes/beacon/config.toml'
-                        # # sftp.put(source,destination)
-                        # sftp.get('keep-nodes/beacon/config/config.toml', 'config1.toml')
-                        # # localpath = 'abc.txt'
-                        # # remotepath = '/opt/crestelsetup/patchzip/abc.txt'
-                        # # sftp.put(localpath, remotepath)
-                        # sftp.close()
-                        # ssh.close()
-                        # file = open('config1.toml', mode='r')
-                        # lines = file.readlines()
-                        # file.close()
-                        # my_dict = {}
-                        # my_list = []
-                        # for line in lines:
-                        #     line = line.split()
-                        #     line = [i.strip() for i in line]
-                        #     st.write(line)
+                    contract = web3.eth.contract(address=address, abi=abi)
+                    minstake = contract.functions.hasMinimumStake(web3.toChecksumAddress(ETH)).call()
+                    if minstake == True:
+                        st.success('Keep Random Beacon Operator Contract Authorized successful')
+                    else:
+                        st.error('Please Authorize on Keep Random Beacon Operator Contract go https://dashboard.test.keep.network/applications/random-beacon')
+
+                    with open('BondedECDSAKeepFactory.json') as r:
+                        data = json.load(r)
+                    abi = data["abi"]
+                    address = web3.toChecksumAddress("0x9EcCf03dFBDa6A5E50d7aBA14e0c60c2F6c575E6")
+                    contract = web3.eth.contract(address=address, abi=abi)
+                    KeepFactory = contract.functions.isOperatorAuthorized(web3.toChecksumAddress(ETH)).call()
+                    if KeepFactory == False:
+                        st.error('Please authorize ECDSAKeepFactory contracts go https://dashboard.test.keep.network/applications/tbtc')
+                    else:
+                        st.success('Authorize ECDSAKeepFactory confifm')
+
+                    with open('KeepBonding.json') as r:
+                        data = json.load(r)
+                    abi = data["abi"]
+                    address = web3.toChecksumAddress("0x60535A59B4e71F908f3fEB0116F450703FB35eD8")
+                    contract = web3.eth.contract(address=address, abi=abi)
+                    wei = contract.functions.unbondedValue(web3.toChecksumAddress(ETH)).call()
+                    unbonded_eth = web3.fromWei(wei, 'ether')
+                    if unbonded_eth == 0:
+                        st.error('Add an amount of ETH to the available balance for Bonding go           https://dashboard.test.keep.network/applications/tbtc')
+                    else:
+                        st.success('Available ETH for bonding %s ETH (recommend >= 20ETH)' % unbonded_eth)
+                    if unbonded_eth != 0 and minstake == True and stake != 0 and stake > 90000 and grand != '' and KeepFactory == True:
+                        st.success('Confirmed')
+                        p.append(private_key)
+                        if st.button('Run'):
+                            # st.write ('GO')
+                            client = paramiko.SSHClient()
+                            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                            client.connect(hostname=host, username=user, password=secret, port=port)
+                            h = '-h'
+                            commands = [
+                                "sudo apt update",
+                                "yes | sudo apt install git -y",
+                                "sudo apt install docker.io curl -y",
+                                "sudo systemctl start docker",
+                                "sudo systemctl enable docker",
+                                "sudo ufw allow 22 && sudo ufw allow 3919 && sudo ufw allow 3920 && yes | sudo ufw enable",
+                                "sudo ufw status",
+
+                                "git clone https://github.com/icohigh/keep-nodes.git",
+                                "echo '%s' >> $HOME/keep-nodes/data/eth-address.txt" % ETH,
+                                "echo '%s' >> $HOME/keep-nodes/data/eth-address-pass.txt" % pass_eth,
+                                "echo '%s' >> $HOME/keep-nodes/data/keep_wallet.json" % json_string,
+                                "echo 'export ETH_PASSWORD=$(cat $HOME/keep-nodes/data/eth-address-pass.txt)' >> $HOME/.profile",
+                                "echo 'export SERVER_IP=$(curl ifconfig.co)' >> $HOME/.profile",
+                                "grep -rl INFURA_BEACON_ID $HOME/keep-nodes/beacon/config* | xargs perl -p -i -e 's/INFURA_BEACON_ID/df8574df74084c71a997f56f137562d0/g'",
+                                "grep -rl INFURA_ECDSA_ID $HOME/keep-nodes/ecdsa/config* | xargs perl -p -i -e 's/INFURA_ECDSA_ID/ab706352c72543af96db73d3b38edad4/g'",
+                                "sed -i 's/.*URL = .*/URL = \"wss:\/\/ropsten.pfk2020.top\/wss\"/g' $HOME/keep-nodes/beacon/config/config.toml",
+                                "sed -i 's/.*URLRPC = .*/URLRPC = \"https:\/\/ropsten.pfk2020.top\/rpc\"/g' $HOME/keep-nodes/beacon/config/config.toml",
+                                "sed -i 's/.*URL = .*/URL = \"wss:\/\/ropsten.pfk2020.top\/wss\"/g' $HOME/keep-nodes/ecdsa/config/config.toml",
+                                "sed -i 's/.*URLRPC = .*/URLRPC = \"https:\/\/ropsten.pfk2020.top\/rpc\"/g' $HOME/keep-nodes/ecdsa/config/config.toml",
+                                "export ETH_PASSWORD=$(cat $HOME/keep-nodes/data/eth-address-pass.txt)",
+                                "sudo docker run -d \
+                            --entrypoint /usr/local/bin/keep-client \
+                            --restart always \
+                            --volume $HOME/keep-nodes/data:/mnt/data \
+                            --volume $HOME/keep-nodes/beacon/config:/mnt/beacon/config \
+                            --volume $HOME/keep-nodes/beacon/persistence:/mnt/beacon/persistence \
+                            --env KEEP_ETHEREUM_PASSWORD=%s \
+                            --env LOG_LEVEL=debug \
+                            --name keep-client \
+                            -p 3919:3919 \
+                            keepnetwork/keep-client:v1.3.0-rc.4 --config /mnt/beacon/config/config.toml start" % pass_eth,
+                                "sudo docker run -d \
+                            --entrypoint /usr/local/bin/keep-ecdsa \
+                            --restart always \
+                            --volume $HOME/keep-nodes/data:/mnt/data \
+                            --volume $HOME/keep-nodes/ecdsa/config:/mnt/ecdsa/config \
+                            --volume $HOME/keep-nodes/ecdsa/persistence:/mnt/ecdsa/persistence \
+                            --env KEEP_ETHEREUM_PASSWORD=%s \
+                            --env LOG_LEVEL=debug \
+                            --name keep-ecdsa \
+                            -p 3920:3919 \
+                            keepnetwork/keep-ecdsa-client:v1.2.0-rc.5 --config /mnt/ecdsa/config/config.toml start" % pass_eth
+                            ]
+
+                            with st.spinner('Wait ... please'):
+                                for command in commands:
+                                    # st.write("-" * 2,command)
+                                    stdin, stdout, stderr = client.exec_command(command)
+                                    print(stdout.read().decode())
+                                    err = stderr.read().decode()
+                                    if err:
+                                        print(err)
+
+                                # print(data)
+                                client.close()
+                            st.success(
+                                'Congratulations, the node is up and running! (please check ECDSA logs on the server itself)')
+                            st.success('Paste on server: sudo docker logs keep-client -f --since 1m')
+                         
+                    else:
+                        st.error("OOPS")
